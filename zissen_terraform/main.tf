491e10b5fc94f2850ea91dc84c3863141ba7c7a7
inagaki-keisuke-lvgs <keisuke.inagaki@leverages.jp>
2025-07-04 15:45:54 +0900

    feat: first

---
+++ zissen_terraform/main.tf
@@ -0,0 +1,9 @@
+
+
+# IAM Role Moduleの使用
+module "describe_regions_for_ec2" {
+  source     = "./iam_role"
+  name       = "describe-regions-for-ec2"
+  identifier = "ec2.amazonaws.com"
+  policy     = data.aws_iam_policy_document.allow_describe_regions.json
+}
